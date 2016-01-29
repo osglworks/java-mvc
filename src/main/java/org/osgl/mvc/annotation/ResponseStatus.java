86deaf573dc7a145fc7b247d1309b70bc5c7e961
Gelin Luo <greenlaw110@gmail.com>
2016-01-30 07:03:22 +1100

    add ResponseStatus annotation

---
+++ src/main/java/org/osgl/mvc/annotation/ResponseStatus.java
@@ -0,0 +1,21 @@
+package org.osgl.mvc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Mark a certain class so when the implementation framework encountered a
+ * return result of the class, it will automatically set the response status
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ResponseStatus {
+    /**
+     * Specify the response status code
+     * @return the response status code
+     * @see org.osgl.http.H.Status
+     */
+    int value();
+}
