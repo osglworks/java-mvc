960005b5eba9c38d64567f26e12d97d3aed38ebb
Gelin Luo <greenlaw110@gmail.com>
2016-08-16 16:12:42 +1000

    add SessionFree annotation

---
+++ src/main/java/org/osgl/mvc/annotation/SessionFree.java
@@ -0,0 +1,16 @@
+package org.osgl.mvc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicate a Controller or a handler method does NOT require
+ * session. This can be used to improve the performance of handling
+ * request for resource that does not require session participation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface SessionFree {
+}
